9de500ace648142ddfab3d5dba2156bc356357f8
LOGANANTH234 <logananthayyasamy@gmail.com>
2023-02-08 22:23:46 +0530

    update

---
+++ java/src/ContainerWithMostWater.java
@@ -0,0 +1,25 @@
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        int arr[]={1,8,6,2,5,4,8,3,7};
+        int start=0;
+        int end= arr.length-1;
+        int breadth=arr.length-1;
+        int max=-1;
+        while(start<end){
+         int small=   Math.min(arr[start],arr[end]);
+          //  System.out.println(small);
+            int water = breadth*small;
+            //System.out.println(water);
+            max =Math.max(max,water);
+            //System.out.println(max);
+            breadth=breadth-1;
+            if(arr[start]<arr[end]){
+                start++;
+            }
+            else {
+                end--;
+            }
+        }
+        System.out.println(max);
+    }
+}
