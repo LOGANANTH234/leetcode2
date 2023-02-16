c78443156102c685b8d0f1f4a7f15c223d2df3f1
LOGANANTH234 <logananthayyasamy@gmail.com>
2023-02-16 23:52:52 +0530

    update

---
+++ java/src/AverageSalaryExcludingtheMinimumandMaximumSalary.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class AverageSalaryExcludingtheMinimumandMaximumSalary {
+    public static void main(String[] args) {
+        int s[]={200,400,100,300};
+        Arrays.sort(s);
+        int sum=0;
+        for(int i=1;i<s.length-1;i++){
+            sum=sum+s[i];
+            //System.out.println(sum);
+        }
+        double b= s.length-2;
+        double ans= sum /b;
+        System.out.println(ans);
+    }
+}
