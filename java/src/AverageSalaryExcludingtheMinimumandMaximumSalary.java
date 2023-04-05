7df0f6903372db07e9a1143d04589ee2d8fe5cd2
LOGANANTH A <105982583+LOGANANTH234@users.noreply.github.com>
2023-04-05 23:05:13 +0530

    Update AverageSalaryExcludingtheMinimumandMaximumSalary.java

--- java/src/AverageSalaryExcludingtheMinimumandMaximumSalary.java
+++ java/src/AverageSalaryExcludingtheMinimumandMaximumSalary.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class AverageSalaryExcludingtheMinimumandMaximumSalary {
     public static void main(String[] args) {
-        int s[]={200,400,100,300};
+        int s[]={200,400,100,300,400};
         Arrays.sort(s);
         int sum=0;
         for(int i=1;i<s.length-1;i++){
